bedd64f19bf0724a6df4f8b9c26a0a7024eb78ae
Saemiru Kim <saemiru.kim@meshkorea.net>
2020-04-08 18:52:22 +0900

    슬기님 피드백 반영 및 settings action 테스트 추가

---
+++ app/src/main/java/net/meshkorea/android/lineheighttest/AddTwoActivity.kt
@@ -0,0 +1,12 @@
+package net.meshkorea.android.lineheighttest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddTwoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_two)
+    }
+}
--- app/src/main/java/net/meshkorea/android/lineheighttest/MainActivity.kt
+++ app/src/main/java/net/meshkorea/android/lineheighttest/MainActivity.kt
@@ -1,11 +1,14 @@
 package net.meshkorea.android.lineheighttest
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.text.SpannableString
+import android.provider.Settings
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
+import io.reactivex.Observable
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,16 +16,59 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val text = getString(R.string.line_height_test)
-        val spanString = SpannableString(text)
+        initListeners()
+    }
 
-        Log.i("TAG", "${resources.displayMetrics.density}")
+    private fun initListeners() {
+        englishButton.setOnClickListener {
+            startActivity(Intent(this, EnglishTestActivity::class.java))
+        }
 
-        // val result =
-        //     buildBackgroundColorSpan(spanString, text, "가져올 수 없습니다.", ContextCompat.getColor(this, R.color.textLine2))
-        // headlineBold.text = result
+        addOneButton.setOnClickListener {
+            startActivity(Intent(this, AddOneActivity::class.java))
+        }
 
-        SpanUtils.setTextWithSpan(this, headlineRegular, ContextCompat.getColor(this, R.color.textLine1), text, 22f)
-        SpanUtils.setTextWithSpan(this, headlineBold, ContextCompat.getColor(this, R.color.textLine2), text, 22f)
+        addTwoButton.setOnClickListener {
+            startActivity(Intent(this, AddTwoActivity::class.java))
+        }
+
+        detailSettingsButton.setOnClickListener {
+            val intent = Intent().apply {
+                action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                data = Uri.Builder().scheme("package").opaquePart(applicationContext.packageName).build()
+            }
+            startActivity(intent)
+        }
+
+        developmentSettings.setOnClickListener {
+            val intent = Intent().apply {
+                action = Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS
+            }
+            startActivity(intent)
+        }
+
+        settingsButton.setOnClickListener {
+            val intent = Intent().apply {
+                action = Settings.ACTION_APPLICATION_SETTINGS
+            }
+            startActivity(intent)
+        }
+
+        notificationSettingsButton.setOnClickListener {
+            val intent = Intent().apply {
+                action = Settings.ACTION_APP_NOTIFICATION_SETTINGS
+                putExtra(Settings.EXTRA_APP_PACKAGE, applicationContext.packageName)
+            }
+            startActivity(intent)
+        }
+
+        channelNotificationSettingsButton.setOnClickListener {
+            val intent = Intent().apply {
+                action = Settings.ACTION_CHANNEL_NOTIFICATION_SETTINGS
+                putExtra(Settings.EXTRA_APP_PACKAGE, applicationContext.packageName)
+                putExtra(Settings.EXTRA_CHANNEL_ID, "")
+            }
+            startActivity(intent)
+        }
     }
 }
